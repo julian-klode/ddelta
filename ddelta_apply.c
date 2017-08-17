363ef64157686e61891df0f954fd9d8c3932ef89
Julian Andres Klode <jak@debian.org>
2017-08-18 01:56:01 +0200

    Add license to ddelta_apply

--- ddelta_apply.c
+++ ddelta_apply.c
@@ -1,6 +1,27 @@
 /* ddelta_apply.c - A sane reimplementation of bspatch
  * 
  * Copyright (C) 2017 Julian Andres Klode <jak@debian.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted providing that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <endian.h>
@@ -121,6 +142,7 @@ int ddelta_apply(FILE *patchfd, FILE *oldfd, FILE *newfd)
     return 0;
 }
 
+#ifndef JKPATCH_NO_MAIN
 int main(int argc, char *argv[])
 {
     if (argc != 4) {
@@ -142,3 +164,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+#endif
