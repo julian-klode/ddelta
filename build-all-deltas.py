13c77824080f4f0a8438ae08032695323a705840
Julian Andres Klode <julian.klode@canonical.com>
2018-07-31 14:52:33 +0800

    Add build-all-deltas.py

---
+++ build-all-deltas.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import apt_pkg, sys, os
+
+apt_pkg.init()
+
+def read_packages_file(path):
+	out = {}
+	with apt_pkg.TagFile(path) as tf:
+		for sec in tf:
+			out[(sec["Package"])] = (sec["Version"], sec["Architecture"], sec["Filename"])
+	return out
+		
+
+release = read_packages_file("dists/%s/main/binary-amd64/Packages.xz" % sys.argv[1])
+updates = read_packages_file("dists/%s/main/binary-amd64/Packages.xz" % sys.argv[2])
+
+for pkg in updates:
+	if pkg not in release:
+		continue
+
+	rel_ver, rel_arch, rel_path = release[pkg]
+	upd_ver, upd_arch, upd_path = updates[pkg]
+
+	print("/home/jak/ddelta/build-delta.sh", rel_path, upd_path, "{}/{}_{}_{}_{}.δdeb".format(os.path.dirname(upd_path), pkg, rel_ver, upd_ver, upd_arch))
